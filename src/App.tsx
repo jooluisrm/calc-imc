9ae16db681418ea08ab1e004e67172be40361347
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-11 18:19:22 -0300

    Fazendo o Disabled nos campos

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ const App = () => {
                         placeholder='Digite a sua altura. Ex: 1.5 (em métros)'
                         value={heightField > 0 ? heightField : ''}
                         onChange={e => setHeightField(parseFloat(e.target.value))}
+                        disabled={toShow ? true: false}
                     />
 
                     <input
@@ -52,9 +53,10 @@ const App = () => {
                         placeholder='Digite o seu peso. Ex: 75.3 (em kg)'
                         value={weightField > 0 ? weightField : ''}
                         onChange={e => setWeightField(parseFloat(e.target.value))}
+                        disabled={toShow ? true: false}
                     />
 
-                    <button onClick={handleCalculateButton}>Calcular</button>
+                    <button onClick={handleCalculateButton} disabled={toShow ? true: false}>Calcular</button>
                 </div>
                 <div className={styles.rightSide}>
                     {!toShow &&
