32576aa749fa23b1799bc87d954c62fbe0f75ea7
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-11 14:51:59 -0300

    Criando o GridItem

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,4 +1,7 @@
 import { Level } from "../../helpers/imc";
+import styles from './GridItem.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
 
 type Props = {
     item: Level;
@@ -6,8 +9,19 @@ type Props = {
 
 export const GridItem = ({ item }: Props) => {
     return (
-        <div>
-            ...
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+                {item.icon === 'up' && <img src={upImage} alt="" width='30' />}
+                {item.icon === 'down' && <img src={downImage} alt="" width='30' />}
+            </div>
+            <div className={styles.gridTitle}>
+                {item.title}
+            </div>
+            <div className={styles.dridInfo}>
+                <>
+                    IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
         </div>
     );
 }
